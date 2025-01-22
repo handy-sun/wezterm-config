5fccb7af66359244c1730791a1ac49cbfbd2cb2f
sooncheer <handy-sun@foxmail.com>
2025-01-22 23:19:53 +0800

    windows saved temp1

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,8 +1,5 @@
-local wezterm = require("wezterm")
-local platform = require("utils.platform")
 
 return {
-    font = wezterm.font("NotoMono NFM"),
     font_size = 12,
 
     --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
