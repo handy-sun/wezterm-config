ad4a1c61207f308c5c9b8ac56d519717a56e4177
sooncheer <handy-sun@foxmail.com>
2026-02-17 12:38:52 +0800

    add pcall to judge module custom exist or not

--- config/init.lua
+++ config/init.lua
@@ -17,16 +17,23 @@ end
 ---@return Config
 function Config:append(new_options)
     for k, v in pairs(new_options) do
-        if self.options[k] ~= nil then
-            wezterm.log_warn(
-                "Duplicate config option detected: ",
-                { old = self.options[k], new = new_options[k] }
-            )
-        else
-            self.options[k] = v
-        end
+        self.options[k] = v
     end
     return self
 end
+
+-- function Config:append(new_options)
+--     for k, v in pairs(new_options) do
+--         if self.options[k] ~= nil then
+--             wezterm.log_warn(
+--                 "Duplicate config option detected: ",
+--                 { old = self.options[k], new = new_options[k] }
+--             )
+--         else
+--             self.options[k] = v
+--         end
+--     end
+--     return self
+-- end
 
 return Config
--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,5 @@
 local Config = require("config")
-
+local wezterm = require("wezterm")
 require("utils.backdrops"):set_files():random()
 
 require("events.right-status").setup()
@@ -7,11 +7,20 @@ require("events.left-status").setup()
 require("events.tab-title").setup()
 require("events.new-tab-button").setup()
 
-return Config:init()
+local opt = Config:init()
     :append(require("config.appearance"))
     :append(require("config.bindings"))
     :append(require("config.fonts"))
     :append(require("config.general"))
     :append(require("config.launch"))
-    :append(require("config.domains")).options
--- append(require("config.domains")) overrrides some default_tab (contains .ssh/config)
+    :append(require("config.domains"))
+
+local has_cus, cus_fun = pcall(require, "custom")
+if has_cus then
+    local cus_tab = cus_fun()
+    wezterm.log_warn("cus_tab: ", cus_tab)
+    return opt:append(cus_tab).options
+else
+    wezterm.log_warn("no custom.lua")
+    return opt.options
+end
