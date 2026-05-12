64db01ce856210fdb0e34d14918064fc98b459cd
sooncheer <handy-sun@foxmail.com>
2026-05-13 03:00:31 +0800

    fix: update WSL:NixOS distribution name to match wsl.exe output

--- config/domains.lua
+++ config/domains.lua
@@ -6,7 +6,7 @@ if platform.is_win then
         wsl_domains = {
             {
                 name = "WSL:NixOS",
-                distribution = "nixwsl",
+                distribution = "nixos", --- 这个值是 wsl.exe -l -v 输出的 distribution 名称
                 default_cwd = "~",
             },
             {
