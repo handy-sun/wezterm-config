ef0522f6b4fdbeeac80b4ea9e6edf891c3d55347
sooncheer <handy-sun@foxmail.com>
2026-05-13 00:03:37 +0800

    feat: support macOS in window frame font size adjustment; increase scrollback lines to 15000

--- config/launch.lua
+++ config/launch.lua
@@ -1,9 +1,6 @@
 local platform = require("utils.platform")()
 
-local options = {
-    default_prog = {},
-    launch_menu = {},
-}
+local options = { }
 
 if platform.is_win then
     options.default_prog = { "pwsh", "-NoLogo" }
