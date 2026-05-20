5d8c21893c8f9d8bcf4774d14a1a68e9d306a13d
sooncheer <handy-sun@foxmail.com>
2026-05-21 00:58:52 +0800

    fix: fit extra.lua of upstream

--- extra.lua
+++ extra.lua
@@ -1,11 +1,5 @@
 local Config = require("config")
-
-require("utils.backdrops"):set_files():random()
-
-require("events.right-status").setup()
-require("events.left-status").setup()
-require("events.tab-title").setup()
-require("events.new-tab-button").setup()
+-- local wezterm = require("wezterm")
 
 local opt = Config:init()
     :append(require("config.appearance"))
@@ -13,6 +7,21 @@ local opt = Config:init()
     :append(require("config.general"))
     :append(require("config.launch"))
     :append(require("config.domains"))
+
+require('utils.backdrops')
+   -- :set_images_dir(require('wezterm').home_dir .. '/Pictures/Wallpapers/')
+   :scan_images_dir()
+   :random()
+
+require('events.left-status').setup()
+require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
+require('events.tab-title').setup({
+   hide_active_tab_unseen = true,
+   unseen_icon = 'numbered_box',
+   show_progress = true,
+})
+require('events.new-tab-button').setup()
+require('events.gui-startup').setup()
 
 -- Home Manager appends this file after defining the generated `config` table.
 for k, v in pairs(opt.options) do
