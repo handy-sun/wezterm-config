6c7b5015774d18a1c809fb4a23c14e227fc0e6ba
sooncheer <handy-sun@foxmail.com>
2026-05-25 18:27:37 +0800

    fix: pgUp/pgDn must with other keys(conflict with nvim etc..)

--- config/bindings.lua
+++ config/bindings.lua
@@ -209,8 +209,8 @@ local keys = {
    -- panes: scroll pane
    { key = 'u',        mods = mod.SUPER, action = act.ScrollByLine(-5) },
    { key = 'd',        mods = mod.SUPER, action = act.ScrollByLine(5) },
-   { key = 'PageUp',   mods = 'NONE',    action = act.ScrollByPage(-0.75) },
-   { key = 'PageDown', mods = 'NONE',    action = act.ScrollByPage(0.75) },
+   { key = 'PageUp',   mods = mod.SUPER_REV,    action = act.ScrollByPage(-0.75) },
+   { key = 'PageDown', mods = mod.SUPER_REV,    action = act.ScrollByPage(0.75) },
 
    -- key-tables --
    -- resizes fonts
